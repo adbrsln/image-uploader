e1ecffcaf05c920a37df9bb73acf758064f8123a
Muhammad Adib Ahmad Roslan <adbroslan@gmail.com>
2020-04-14 21:30:33 +0800

    added config files

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  //if you host the project in a folder overwrite below else, leave it blank
+  $folder = '';
+  //set your username and password if u want to use username password
+  $username = '';
+  $password = '';
+?>
